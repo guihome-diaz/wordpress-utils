6ba57f5cce7fad1029b282d462f0735ab0be55c6
Guillaume <guillaume@qin-diaz.com>
2020-12-14 22:50:47 +0100

    project start

---
+++ wordrpess-utils/src/main/java/eu/daxiongmao/wordrpess/WordrpessUtilsApplication.java
@@ -0,0 +1,13 @@
+package eu.daxiongmao.wordrpess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordrpessUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordrpessUtilsApplication.class, args);
+	}
+
+}
---
+++ wordrpess-utils/src/test/java/eu/daxiongmao/wordrpess/WordrpessUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.daxiongmao.wordrpess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordrpessUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
