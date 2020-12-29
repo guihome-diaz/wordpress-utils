daa406d1a917221dd398ac25bcf1485125e9b827
Guillaume <guillaume@qin-diaz.com>
2020-12-29 13:07:59 +0100

    Comment meta entity, without foreign key

--- wordrpess-utils/src/main/java/eu/daxiongmao/wordpress/db/model/CommentMeta.java
+++ wordrpess-utils/src/main/java/eu/daxiongmao/wordpress/db/model/CommentMeta.java
@@ -8,19 +8,45 @@ import java.io.Serializable;
 
 /**
  * Wordpress "commentmeta".
- * This is the link between comments and related metadata
+ * These are the comments' metadata
  * @version 1.0
  * @since 2020/12
  */
 @Data
 @Entity
-@Table(name = "commentmeta")
+@Table(name = "commentmeta",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "commentmeta_uq_ID", columnNames = "meta_id")
+        },
+        indexes = {
+                @Index(name = "commentmeta_idx_id", columnList = "meta_id", unique = true),
+                @Index(name = "commentmeta_idx_comment_id", columnList = "comment_id asc"),
+                @Index(name = "commentmeta_idx_meta_key", columnList = "meta_key asc")
+        }
+)
 public class CommentMeta implements Serializable {
 
+    /** technical identifier */
     @NonNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "meta_id", updatable = false, nullable = false)
     private Integer id;
+
+    /** Related comment */
+    // FIXME adjust relationship @ManyToOne (Unidirectional association)
+    @NonNull
+    @Column(name = "comment_id", nullable = false)
+    private Integer commentId;
+
+    /** Metadata key */
+    @NonNull
+    @Column(name = "meta_key", nullable = false, length = 255)
+    private String key;
+
+    /** Metadata value */
+    @NonNull
+    @Column(name = "meta_value", nullable = false, length = 65535)
+    private String value;
 
 }
